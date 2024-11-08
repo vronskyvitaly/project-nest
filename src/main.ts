d437ee0cb25ef0598658bc81bf3637dcd31edf66
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-11-08 23:35:14 +0300

    first c

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+  });
   await app.listen(process.env.PORT ?? 3020);
 }
 bootstrap();
