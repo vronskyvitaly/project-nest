4c4dd11e976b5a378670cb920a0fe57cafa9c4df
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-11-08 23:59:50 +0300

    first c

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-  await app.listen(process.env.PORT ?? 3020);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  await app.listen(3000);
 }
+
 bootstrap();
