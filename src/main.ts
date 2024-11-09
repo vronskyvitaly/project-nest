e3da1b701ad530b76ce68ac2d5c0675c76905b22
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-11-09 11:13:03 +0300

    first c

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const options = {
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     preflightContinue: true,
+//     optionsSuccessStatus: 204,
+//     credentials: true,
+//   };
+//   app.enableCors(options);
+//   await app.listen(3020);
+// }
+// bootstrap();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
-  app.enableCors(options);
-  await app.listen(3000);
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+    }),
+  );
+  await app.listen(3020);
 }
 bootstrap();
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule, new FastifyAdapter());
-//   app.enableCors();
-//   await app.listen(3000);
-// }
-// bootstrap();
