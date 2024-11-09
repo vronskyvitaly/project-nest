7b94002c520c0611d202ab5184aa323d3e1bd82f
vronsky vitaly <94834175+vronskyvitaly@users.noreply.github.com>
2024-11-09 12:10:57 +0300

    first c

--- src/main.ts
+++ src/main.ts
@@ -18,11 +18,17 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(
-    helmet({
-      crossOriginEmbedderPolicy: false,
-    }),
-  );
+  app.enableCors({
+    origin: true, // Add your frontend URL here
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
+  });
   await app.listen(3020);
 }
 bootstrap();
